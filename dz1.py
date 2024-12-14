9ed21ef4103d0b0ca81c9358389cdba142f858a9
Emesiland <emesiland@mail.ru>
2024-12-14 13:39:22 +0400

    Добавил str для Менторов

--- dz1.py
+++ dz1.py
@@ -16,7 +16,30 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
-        
+
+    def average_grade(self):
+        total_sum = sum(sum(grade_list) for grade_list in self.grades.values())
+        total_count = sum(len(grade_list) for grade_list in self.grades.values())
+        return round(total_sum / total_count, 1) if total_count > 0 else 0.0
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.average_grade()}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}'
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            raise TypeError("Можно сравнивать только студентов.")
+        return self.average_grade() < other.average_grade()
+
+    def __gt__(self, other):
+        if not isinstance(other, Student):
+            raise TypeError("Можно сравнивать только студентов.")
+        return self.average_grade() > other.average_grade()
+
+    def __eq__(self, other):
+        if not isinstance(other, Student):
+            raise TypeError("Можно сравнивать только студентов.")
+        return self.average_grade() == other.average_grade()
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
