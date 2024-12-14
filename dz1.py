e19ddf03351672c69c5807e03b618780ed9dd259
Emesiland <emesiland@mail.ru>
2024-12-14 13:17:23 +0400

    Добавил примеры работы кода

--- dz1.py
+++ dz1.py
@@ -6,6 +6,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.lecture_grades = {}
+
+    def add_lecture_grade(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
         
 class Mentor:
     def __init__(self, name, surname):
