c2372da6f624b5ef56e61a2b959ae1b58bebc377
Emesiland <emesiland@mail.ru>
2024-12-14 13:48:53 +0400

    Добавил str и возможность сравнения для лекторов

--- dz1.py
+++ dz1.py
@@ -45,6 +45,9 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+    
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
