87d7c69a6eb46f1f08052f59848f6358dd418597
Emesiland <emesiland@mail.ru>
2024-12-14 14:04:29 +0400

    Добавил подсчёт средних оценок

--- dz1.py
+++ dz1.py
@@ -112,3 +112,55 @@ best_student.add_lecture_grade(cool_lecturer, 'Python', 9)
 
 print(f'Оценки студента: {best_student.grades}')
 print(f'Оценки лектора: {cool_lecturer.grades}')
+
+student1 = Student('Ivan', 'Ivanov', 'Male')
+student1.courses_in_progress += ['Python', 'Git']
+student1.finished_courses += ['Введение в Python']
+
+student2 = Student('Kate', 'Mamaeva', 'Female')
+student2.courses_in_progress += ['Python', 'Java'] 
+student2.finished_courses += ['Алгоритмы и структуры данных']
+
+reviewer1 = Reviewer('Some', 'Buddy')
+reviewer1.courses_attached += ['Python', 'Git']
+
+lecturer1 = Lecturer('Jim', 'Green')
+lecturer1.courses_attached += ['Python', 'Git']
+
+lecturer2 = Lecturer('Anna', 'Blue')
+lecturer2.courses_attached += ['Python', 'Java'] 
+
+reviewer1.rate_hw(student1, 'Python', 9)
+reviewer1.rate_hw(student1, 'Python', 10)
+reviewer1.rate_hw(student1, 'Git', 8)
+reviewer1.rate_hw(student1, 'Git', 9)
+
+reviewer1.rate_hw(student2, 'Python', 8)
+reviewer1.rate_hw(student2, 'Java', 9) 
+
+student1.add_lecture_grade(lecturer1, 'Python', 7)
+student1.add_lecture_grade(lecturer1, 'Python', 8)
+student1.add_lecture_grade(lecturer1, 'Python', 9)
+
+student2.add_lecture_grade(lecturer2, 'Python', 6)
+student2.add_lecture_grade(lecturer2, 'Java', 7)
+
+print(student1)
+print(student2)
+print(reviewer1)
+print(lecturer1)
+print(lecturer2)
+
+if student1 > student2:
+    print("Первый студент лучше второго")
+elif student1 < student2:
+    print("Второй студент лучше первого")
+else:
+    print("Они равны")
+
+if lecturer1 > lecturer2:
+    print("Первый лектор лучше второго")
+elif lecturer1 < lecturer2:
+    print("Второй лектор лучше первого")
+else:
+    print("Они равны")
